7f0c097eeeda04768918be10ee41c0dd234b6960
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-04 16:37:02 +0100

    commit

--- Assets/Scripts/PipeMovement.cs
+++ Assets/Scripts/PipeMovement.cs
@@ -6,15 +6,16 @@ public class PipeMovement : MonoBehaviour
 {
 
     public float _moveSpeed = 5f;
-
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float _deadZone = -50f;
 
     void Update()
     {
+
+        // move pipe from right to left
         transform.position = transform.position + (Vector3.left * _moveSpeed) * Time.deltaTime;
+        
+        // destroy pipe once pos.x hits dead zone
+        if (transform.position.x <= _deadZone)
+            Destroy(gameObject);
     }
 }
--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-
+    [Header("References")]
     public GameObject _pipe;
+
+    [Header("Spawn Function")]
     public float _spawnRate = 2;
     private float _timer = 0;
+    [SerializeField] private float _heightOffset = 10f;
 
 
     void Start()
@@ -32,6 +35,8 @@ public class PipeSpawner : MonoBehaviour
 
     private void SpawnPipe()
     {
-        Instantiate(_pipe, transform.position, transform.rotation);
+        float _lowestPoint = transform.position.y - _heightOffset;
+        float _highestPoint = transform.position.y + _heightOffset;
+        Instantiate(_pipe, new Vector3(transform.position.x, Random.Range(_lowestPoint, _highestPoint), 0), transform.rotation);
     }
 }
