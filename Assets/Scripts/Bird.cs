c0387aee5af305c4f4162313fdf8303b2fa8162c
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-05 08:45:19 +0100

    commit

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
 
-    // references
     [Header("References")]
     public Rigidbody2D _rb;
     public LogicManager logic;
@@ -13,26 +12,42 @@ public class Bird : MonoBehaviour
     [Header("States")]
     public bool _isBirdAlive = true;
 
-    // references
     [Header("Flap")]
     public float _flapStrength;
 
+    [Header("Y Death Zone")]
+    [SerializeField] private float _yDeathZone;
 
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManager>();
     }
 
-
     void Update()
+    {
+        YDeathZone();
+        FlapMovement();
+    }
+
+    private void FlapMovement()
     {
         if (Input.GetKeyDown(KeyCode.Space) && _isBirdAlive)
             _rb.velocity = Vector2.up * _flapStrength;
+    }
+
+    private void YDeathZone()
+    {
+        if (Mathf.Abs(transform.position.y) > _yDeathZone)
+        {
+            _isBirdAlive = false;
+            logic.GameOver();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         logic.GameOver();
         _isBirdAlive = false;
-    }
+    } 
+
 }
